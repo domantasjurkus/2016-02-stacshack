5af0063792f839a7b7445913a04fe4542db5bba5
skaiste <butkute.skaiste@gmail.com>
2016-02-21 02:45:53 +0000

    Add function for converting

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ $(function() {
 	});
 
 });
+
+function convert(code) {
+	code = code.replace("\n", ";\n ");
+	var codeWords = code.split(" ");
+	
+	for (var i = 0; i < codeWords.length; i++) {
+		for (var j in words) {
+			if (words.hasOwnProperty(j) && (codeWord[i] == j)){
+				codeWord[i] = words[j];
+				break;			
+			}
+		}
+	}
+	return codeWords.join(" ");
+}
